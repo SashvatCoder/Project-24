cb77c707899ca52ae05724858960dc1b4ea91862
SashvatCoder <77525587+SashvatCoder@users.noreply.github.com>
2021-05-06 15:40:33 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,26 @@
+class Ground{
+contructor(x,y,width,height){
+var options={
+isStatic:true
+
+}
+
+
+this.body=Bodies.rectangle(x,y,width,height,options)
+this.x=x
+this.y=y
+this.width=width
+this.height=height
+World.add(world,this.body)
+}
+display(){
+var pos=this.body.position
+push()
+translate(pos.x,pos.y)
+rectMode(CENTER)
+strokeWeight(4)
+fill(255,255,0)
+rect(0,0,this.width,this.height)
+pop()
+}
+}
